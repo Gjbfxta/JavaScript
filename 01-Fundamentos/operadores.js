1b3424072bde3af04acce8c48a61b3b72cc9e733
Gjbfxta <166183187+Gjbfxta@users.noreply.github.com>
2024-09-23 13:48:38 -0600

    Operadores

---
+++ 01-Fundamentos/operadores.js
@@ -0,0 +1,45 @@
+/* Son simbolos utilizados para realizar openaciones sobre valores y variables
+
+1. Aritmeticos
+2.Asignación
+3.Comparación */
+/* 
+Operadoreres Aritmeticos
+  se utilizan calculos matematicos
+*/
+// Suma (+) suam dos valores
+let suma = 20 + 16;
+console.log(suma);
+// resta (-)
+
+let resta = 25 - 46;
+console.log(resta);
+
+// Multiplicaccion (*)
+
+let multiplicacion = 25 * 46;
+console.log(multiplicacion);
+
+//Divisoin (/)
+
+let division = 32 / 4;
+console.log (division);
+
+//Modulo(%) devuelve el resto de la division de los valores.
+
+let mod = 7 % 3;
+console.log(mod);
+
+// Exponentes (**)
+let expo = 4 ** 15;
+console.log (expo)
+
+//incremento (++) por una unidad
+let x = 15;
+x ++;
+console.log (x);
+
+//Decremento (--)
+let y = 4;
+y --;
+console.log(y)
