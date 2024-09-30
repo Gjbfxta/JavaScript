a99a91cf7c6006500688129fe161a455ed39d0d2
Gjbfxta <166183187+Gjbfxta@users.noreply.github.com>
2024-09-30 11:50:50 -0700

    Asignacion y Comparacion

--- 01-Fundamentos/operadores.js
+++ 01-Fundamentos/operadores.js
@@ -43,3 +43,98 @@ console.log (x);
 let y = 4;
 y --;
 console.log(y)
+
+
+/*
+
+2. Asignación
+
+  Estos operadores asignan o reasigan un valor a una variable.
+
+*/
+
+// Asignación (=): Asigna un valor a una variable.
+
+let w = 95;
+
+// Asignación con suma (+=): Suma el valor de una variable y la reasigna.
+
+w += 56; 
+console.log(w);
+
+// Asignación con resta (-=): Resta el valor a una variable y la reasigna.
+
+w -=34; 
+console.log(w);
+
+// Asignación con Multiplicación (*=):Multiplica una variable por un valor y la asigna
+
+w *=5; // 70
+console.log(w);
+
+// Asignación con división (/=): Divide una varible con un valor y la reasinga.
+
+w /=22;
+console.log(w);
+
+/* 
+
+3. Comparación
+
+  Estos operadores comparan dos valores y devuelven 'true' o 'false'.
+
+*/
+
+// Igualdad (==): Compara dos valores para ver si son iguales. NOTA: Sin tomar en cuenta el tipo de dato.
+
+let valorUNO = "2";
+let compara = 2 == valorUNO;
+
+console.log(compara) // true
+
+// Igualdad estricta (===): Compara dos valore y su tipo, para ver si son exactamente iguales.
+
+compara = 2 === valorUNO;
+
+console.log(compara) // false
+
+// Desigualdad (!=): Verificar si dos valores no son iguales. NOTA: No tom en cuenta el tipo de dato.
+
+let valorDOS = "40";
+let valorTRES = 40;
+
+console.log(valorDOS != valorTRES);
+
+// Desiguladd estricta (!==) Verifica si dos valores y sus tipos no son exactamente sus iguales.
+
+valorDOS = "40";
+valorTRES = 40;
+
+console.log(valorDOS !== valorTRES) // True
+
+// Mayor que (>): Verifica si el primer valor es mayor que el segundo.
+
+compara = 2 > 4;
+
+console.log(compara); // false
+
+// Mayor o igual que (>=): Verifica si el primer valor es mayor o igual que el segundo.
+
+compara = 4 >= 4;
+
+console.log(compara); // true
+
+// Menor que (<): Verifica si el primer valor es menor que el segundo
+
+let Dimitri = 30;
+let SuCrush = 8;
+
+console.log (SuCrush < Dimitri); // true
+console.log(10 < 6); // false
+
+// Menor o igual que (<=): Verifica si el primer valro es menor o igual que el segundo valor
+
+console.log(5 <= 20); //true
+console.log(40 <= 150); //true
+console.log(58 <= 6); // false
+console.log(4 <= 4); // true
