a549f3817eda3c0623cbe0aa5bc8ea0558642e92
Gjbfxta <166183187+Gjbfxta@users.noreply.github.com>
2024-10-02 12:50:43 -0600

    practica

---
+++ 01-Fundamentos/practica.js
@@ -0,0 +1,13 @@
+
+
+let edad = prompt ("¿que edad tines?");
+console.log(edad);
+
+if (edad>=18){
+  alert ("Eres mayor de edad")
+}else if (edad<18){
+  alert("Eres menor de edad")
+} else {
+  alert ("Eres un alien")
+}
+12
