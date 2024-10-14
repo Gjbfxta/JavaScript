4bdf41e57e8dd3266257ee6320e2411f02bccbe9
Gjbfxta <166183187+Gjbfxta@users.noreply.github.com>
2024-10-14 13:55:01 -0600

    Operadores ternarios

---
+++ 01-Fundamentos/operador-ternario.js
@@ -0,0 +1,24 @@
+/* let edad = 23;
+
+if (edad>= 18 ){
+  console.log ("eres mayor de edad")
+} else {
+  console.log ("eres un bb ")
+}
+ */
+/* 
+
+trabaja con tres operadores
+
+1. una condicion 
+2. la expresion si la condicion es verdaera
+3. L expresion si la condicion es falsa
+
+sintaxis
+condicion ? expresionsiverdadera : expresionsifalsa
+ */
+
+let edad = 3;
+let resultado = edad >= 18 ? "eres mayor de edad xd" : "eres un bb"
+console.log (resultado);
+
